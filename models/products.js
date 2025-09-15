398e1f0f2d741a59d9b66c26748f0aabfe85da7d
Leandro Amaro Garcia <leandroamarogarcia@leandros-mbp.lan>
2025-09-15 10:51:03 -0300

    added products and categories entities

--- models/products.js
+++ models/products.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
   name: { type: String },
+  categories: [{ type: Schema.Types.ObjectId, ref: "Category" }]
 });
 
 export default mongoose.model("Product", productSchema, "Products");
